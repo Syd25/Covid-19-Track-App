482bcc0deaf12b9316cb50fc0aa7e026ec3c7c61
unknown <chughsiddharth25@gmail.com>
2020-09-03 01:37:41 +0530

    final commit

---
+++ build/precache-manifest.e0ead913c8f322fedac6549a1fa2c09c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce628b33d4dfd1e1a6b899c2dc16a598",
+    "url": "/covid19trackapp/index.html"
+  },
+  {
+    "revision": "ad719d20aa40e5966fd1",
+    "url": "/covid19trackapp/static/css/main.4564d54b.chunk.css"
+  },
+  {
+    "revision": "371873f086c9c03e276c",
+    "url": "/covid19trackapp/static/js/2.d97a4c81.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19trackapp/static/js/2.d97a4c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad719d20aa40e5966fd1",
+    "url": "/covid19trackapp/static/js/main.4b215bfd.chunk.js"
+  },
+  {
+    "revision": "a398d0885521b403197c",
+    "url": "/covid19trackapp/static/js/runtime-main.810f75e1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19trackapp/static/media/image.d7265326.png"
+  }
+]);
